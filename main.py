e144da20e6a0671227f5abebeb9e16b00a6e4c58
Eugene <evgkisevgkis@gmail.com>
2024-05-12 22:09:27 +0500

    создание файла main.py

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from config import config
-from utils import create_database, get_vacancies, insert_data, dbmanager_interaction
+from utils import create_database, get_vacancies, insert_data, dbmanager_interaction, DBManager
+import psycopg2
 
 
 def main():
+    """Запуск функционала и взаимодействие с пользователем"""
     db_name = 'hh_vacancies'
     params = config()
-    companies = []
+    companies = ['Платформа Больших Данных',
+                 'ООО Центр пространственных исследований',
+                 'ООО Автомакон',
+                 'ИТЕРАНЕТ',
+                 'evrone.ru',
+                 'Aston',
+                 'рунити',
+                 'Тинькофф',
+                 'ООО Перфект Системс',
+                 'ООО Итон',
+                 'Outlines Technologies',
+                 'ООО Онли',
+                 'ООО Смартекс',
+                 'Центр технологий моделирования']
 
-    create_database(params, db_name)
+    if input('Желаете ли создать новую базу данных?(1=да) ') == '1':
+        print('Хорошо')
+        create_database(params, db_name)
+        print('База данных создана')
+        data = get_vacancies(companies)
+        print('Данные получены')
+        with psycopg2.connect(dbname=db_name, **params) as conn:
+            with conn.cursor() as cur:
+                insert_data(cur, data)
+        print('Полученные данные вставлены в базу данных')
+    dbman = DBManager(db_name, params)
+    while True:
+        dbmanager_interaction(dbman)
+        if input('Введите 0 для выхода ') == '0':
+            break
 
-    data = get_vacancies(companies)
-    insert_data(data)
 
-    dbmanager_interaction()
+if __name__ == '__main__':
+    main()
